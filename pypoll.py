58c5ff758f4ced88c4ae77fd57895d9f6cf102e4
AmyLuc <91698325+AmyLuc@users.noreply.github.com>
2021-11-13 18:13:35 -0500

    Update pypoll.py

--- pypoll.py
+++ pypoll.py
@@ -1,24 +1,13 @@
-#Find the total of votes cast.
-
-#Make a complete list of candidates who received votes.
-
-#List how many votes each candiddate recieved.
-
-#Find percentage of votes each cadidate won.
-
-#State the winner based on the results.
-
-#cd Desktop/Data_Class/Class_Work/other_HW/Module3/Election_Analysis
-
-#python3
-
+#Import dependencies.
 import csv
 import numpy as np
 import os
 
+#Initialize paths to relevent files.
 file_to_load = 'election_results.csv'
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#Set necessary variables, lists, dictionaries, etc.
 total_votes = 0
 candidate_options = []
 candidate_votes = {}
@@ -26,14 +15,23 @@ winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 
-# Using the with statement open the file as a text file.
+counties = []
+county_turnout = {}
+highest_turnout = 0
+populous = ""
+largest_percentage = 0 
+
+#Open file with data. 
 with open(file_to_load) as elec_data:
     file_reader = csv.reader(elec_data)
     headers = next(file_reader)
+    #count rows for votes.
     for row in file_reader:
         total_votes += 1
+        #get rows for candidates and counties
         candidate_name = row[2]
-
+        county_name = row[1]
+      
 
 #Cand.Options is an empty list. Candidate names are the third (0,1,2) 
 #category in the csv. This adds any name it finds that hasn't been added
@@ -48,13 +46,58 @@ with open(file_to_load) as elec_data:
 #so that it actually iterates through the names and counts them.
         candidate_votes[candidate_name] += 1
 
-    for candidate_name in candidate_votes:
+        if county_name not in counties:
+            counties.append(county_name)
+            county_turnout[county_name] = 0
+        county_turnout[county_name] += 1
+
+
+    #Write our results to a text file.
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+       
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+
+    #Make a list of unique counties.
+    for county_name in county_turnout:
+        turnout = county_turnout[county_name]
+        #Find the number of votes from each country and the percentage of votes that each county
+        #contributed.
+        turnout_percentage = float(turnout)/ float(total_votes)* 100
+        print(f"{county_name}: contributed {turnout_percentage:.1f}% to the total voter turnout")
+
+        county_results = (f"{county_name}: {turnout_percentage:.1f}% ({turnout:,})\n")
+        #Write results to a text file.
+        txt_file.write(county_results)
+
+        #Find and print county with largest voter turnout. State the numbers and percentages.
+        if (turnout > highest_turnout) and (turnout_percentage > largest_percentage):
+            highest_turnout = turnout
+            largest_percentage = turnout_percentage
+            populous = county_name
+    turnout_summary = (
+        f"-------------------------\n"
+        f"County with largest voter turnout: {populous}\n"
+        f"{populous}'s turnout: {highest_turnout:,}\n"
+        f"{populous}'s contribution to total voter turnout: {largest_percentage:.1f}%\n"
+        f"-------------------------\n")
+    txt_file.write(turnout_summary)
+
+    #The code bellow does the same thing as the code above, just with the data from the candidates row,
+    #instead of the counties row.
+    for candidate_name in candidate_votes: 
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes)/ float(total_votes)*100
-    #!!!!! fix this from 3.4.11 so that the percentages aren't ridiculous to read
-        print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+        print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
 
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        txt_file.write(candidate_results)
 
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
@@ -66,8 +109,10 @@ with open(file_to_load) as elec_data:
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
-    print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
+               
 
 print(candidate_options)
 print(candidate_votes)
+#Ta da!
 
